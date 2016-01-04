5ccc1ae71bb49ca5d3c27cda7132fdde62c67e1c
Mark Finn <mfinn@fedex.com>
2016-01-04 14:31:23 -0600

    mqtt starting

--- script2.lua
+++ script2.lua
@@ -6,13 +6,15 @@ dispaddr = 0x3c
 
 relaypin=8
 
+devid = "thermostat_"..node.chipid()
+prefix = "/IoTmanager/"..devid.."/"
 
 
 function relay(state)
     gpio.write(relaypin, state and gpio.HIGH or gpio.LOW) 
 end
 
-function disptemp(t, f)
+function convtemp(t, f)
     if not t then
         return "XX"
     elseif f then
@@ -34,10 +36,69 @@ function setup()
     
     timeouts={}
 
-    
-    
+    mq=mqtt.Client(devid, 30)
+    mq:lwt(prefix.."lwt", "offline", 1, 0)
+    mqstat=0
+    mqtmrdn=0
+    mqtmrup=0
+
     timeoutUpdate("temp", 10)
 end
+
+function mqttHandle(conn, topic, data) 
+  mqtmrdn=tmr.now()
+  print(topic .. ":" ) 
+  if data ~= nil then
+    print(data)
+  end
+end
+
+function doManageMqtt()
+
+    if mqstat~=5 and wifi.sta.status() == wifi.STA_GOTIP then
+        if not pcall(function()
+            if mqstat > 1 then
+                mq:close()
+                mqstat=1
+                tmr.alarm(3, 2000, 1, doManageMqtt) 
+                return
+            end
+            mq:on("offline", function(con) 
+                mqstat=1 
+                mq:close()
+                tmr.alarm(3, 2000, 1, doManageMqtt) 
+            end)
+            mq:on("message", mqttHandle)
+            mqstat=2
+            mqtmrup=tmr.now()
+            mq:connect("192.168.13.3", 1883, 0, function(conn) 
+                mqstat=3
+                mqtmrdn=tmr.now()
+                mqtmrup=tmr.now()
+                tmr.alarm(3, 2000, 1, doManageMqtt) 
+                mq:subscribe(prefix.."#",1, function(conn) 
+                    mqstat=4
+                    mqtmrdn=tmr.now()
+                    mqtmrup=tmr.now()
+                    tmr.alarm(3, 2000, 1, doManageMqtt) 
+                    mq:subscribe("/IoTmanager",1, function(conn) 
+                        mqstat=5
+                        mqtmrdn=tmr.now()
+                        tmr.alarm(3, 15000, 1, doManageMqtt) 
+                    end) 
+                end) 
+            end)
+        end) then
+            mqstat=1 
+            mq:close()
+            tmr.alarm(3, 2000, 1, doManageMqtt) 
+        end
+    end
+end
+--=mqstat
+--=prefix
+--tmr.stop(3)
+--doManageMqtt()
 
 function timeoutUpdate(s, delay)
     timeouts[s]=tmr.now()/1000000+delay
@@ -56,7 +117,6 @@ function doTemp()
          end
 
          timeoutUpdate("temp", 10)
-         doScreen()
     end
 end
 
@@ -77,46 +137,71 @@ disp:drawXBM(x, y-12, 12, 12, "\68\4\238\14\68\4\34\2\34\2\34\2\68\4\136\8\136\8
 end
 
 function iconWifi(x,y, stat)
-if stat == wifi.STA_IDLE then return
-elseif stat == wifi.STA_GOTIP then
-disp:drawXBM(x, y-12, 12, 11, "\240\0\156\3\6\6\243\12\152\1\6\6\96\0\152\1\0\0\96\0\96\0")
--- f000 000011110000
--- 9c03 001110011100
--- 0606 011000000110
--- f30c 110011110011
--- 9801 000110011000
--- 0606 011000000110
--- 6000 000001100000
--- 9801 000110011000
--- 0000 000000000000
--- 6000 000001100000
--- 6000 000001100000
+if stat == wifi.STA_IDLE then
+	return
 
 elseif stat == wifi.STA_CONNECTING then
-disp:drawXBM(x+1+4*math.floor((tmr.now()/333333)%3), y-12+9, 2, 2, "\3\3")
+	disp:drawXBM(x+1+4*math.floor((tmr.now()/333333)%3), y-12+9, 2, 2, "\3\3")
 
 else
-disp:drawXBM(x, y-12, 12, 12, "\240\0\156\3\6\6\243\12\152\1\6\4\96\14\152\14\0\14\96\14\96\0\0\14")
--- f000 000011110000
--- 9c03 001110011100
--- 0606 011000000110
--- f30c 110011110011
--- 9801 000110011000
--- 0604 011000000010
--- 600e 000001100111
--- 980e 000110010111
--- 000e 000000000111
--- 600e 000001100111
--- 6000 000001100000
--- 000e 000000000111
+	disp:drawXBM(x, y-12, 12, 11, "\240\0\156\3\6\6\243\12\152\1\6\6\96\0\152\1\0\0\96\0\96\0")
+	-- f000 000011110000
+	-- 9c03 001110011100
+	-- 0606 011000000110
+	-- f30c 110011110011
+	-- 9801 000110011000
+	-- 0606 011000000110
+	-- 6000 000001100000
+	-- 9801 000110011000
+	-- 0000 000000000000
+	-- 6000 000001100000
+	-- 6000 000001100000
+
+	if stat ~= wifi.STA_GOTIP then
+		disp:drawXBM(x+8, y-12+5, 4, 7, "\4\14\14\14\14\0\14")
+		-- 04 0010
+		-- 0e 0111
+		-- 0e 0111
+		-- 0e 0111
+		-- 0e 0111
+		-- 00 0000
+		-- 0e 0111
+	end
 end
+
+function iconMqtt(x,y, stat, u, d)
+	if stat==5 then
+		disp:drawXBM(x, y-12, 12, 8, "\254\3\255\7\3\6\3\6\3\6\3\6\254\3\255\7")
+		-- fe03 011111111100
+		-- ff07 111111111110
+        -- 0306 110000000110
+        -- 0306 110000000110
+        -- 0306 110000000110
+        -- 0306 110000000110
+        -- ff07 111111111110
+        -- fe03 011111111100
+	end
+    if u then
+        disp:drawXBM(x, y-12+8, 5, 3, "\4\14\31")
+    -- 04 00100 0111110
+    -- 0e 01110 0011100
+    -- 1f 11111 0001000
+    end
+    if d then
+        disp:drawXBM(x+5, y-12+8, 7, 3, "\227\192\128")
+    -- e3 00100 0111110
+    -- c0 01110 0011100
+    -- 80 11111 0001000
+    end
+end
+
 
 end
 
 function doScreen()
     tmr.stop(2)
-     local sp=disptemp(setpoint, fahrenheit).."\176"
-     local curr=disptemp(temperature, fahrenheit).."\176  "..disptemp(humidity).."%"
+     local sp=convtemp(setpoint, fahrenheit).."\176"
+     local curr=convtemp(temperature, fahrenheit).."\176  "..convtemp(humidity).."%"
      disp:setFont(u8g.font_fub30)
      local spw = disp:getStrWidth(sp)
      disp:setFont(u8g.font_9x15)
@@ -133,9 +218,12 @@ function doScreen()
          end
          dispwstat = wifi.sta.status()
          iconWifi(disp:getWidth()-12,disp:getHeight(), dispwstat)
+         dispmqstat = mqstat
+         iconMqtt(disp:getWidth()-12-12,disp:getHeight(), dispmqstat, mqstatu, mqstatd)
+
     until disp:nextPage() == false
-     timeoutUpdate("screen", 3)
-    tmr.alarm(2, 1000, 0, doScreen) 
+     timeoutUpdate("screen", 10)
+     disptime=tmr.now()
 end
 
 
@@ -157,7 +245,30 @@ setup()
 doScreen()
 tmr.alarm(0, 400, 1, doWdt) 
 tmr.alarm(1, 333, 1, doTemp) 
-tmr.alarm(2, 100, 1, function() if dispwstat ~= wifi.sta.status() or dispwstat == wifi.STA_CONNECTING then doScreen() end end ) 
+tmr.alarm(2, 100, 1, function() 
+	newmqstatd = tmr.now() - mqtmrdn < 1000000
+	newmqstatu = tmr.now() - mqtmrup < 1000000
+	if tmr.now() - disptime > 1000000
+	or disptemp ~= temp
+    or disphumidity ~= humidity
+    or dispsetpoint ~= setpoint
+	or dispwstat ~= wifi.sta.status() 
+	or dispwstat == wifi.STA_CONNECTING
+	or dispmqstat ~= mqstat
+	or newmqstatd ~= mqstatd  
+	or newmqstatu ~= mqstatu  
+	then 
+        print("here")
+		mqstatd = newmqstatd  
+		mqstatu = newmqstatu  
+        disptemp = temp
+        disphumidity = humidity
+        dispsetpoint = setpoint
+		doScreen() 
+	end 
+end ) 
+tmr.alarm(3, 1000, 1, doManageMqtt) 
+
 
 --=adc.readvdd33()
 --wifi.sta.connect()

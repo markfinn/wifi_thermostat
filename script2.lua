0f9d7473a9c0c690b8af93b21932e72486c63519
Mark Finn <mfinn@fedex.com>
2016-01-06 20:43:51 -0600

    telnet server, rotary encoder, slower temp loop

--- script2.lua
+++ script2.lua
@@ -25,10 +25,38 @@ function disptemp(t, f)
      return string.format("%3d", t+.5)
 end
 
+
+
 function setup()
     gpio.mode(relaypin, gpio.OUTPUT)
     relay(false)
+do
+    local encastate
+    local encbstate
+    local encpos=0
+    local function enccb(cw)
+       encpos = encpos + (cw and 1 or -1)
+       print(encpos)
+    end
+
+    function enccbsw(pushed)
+       print(pushed)
+    end
+
+    local function xor(a,b)
+    if a and b or not a and not b then return false end
+    return true
+    end
     
+    gpio.mode(encsw, gpio.INT, gpio.PULLUP)
+    gpio.trig(encsw, "both", function(level) enccbsw(level==0) end)
+    gpio.mode(encqa, gpio.INT, gpio.PULLUP)
+    gpio.trig(encqa, "both", function(level) level=gpio.read(encqa)==0 enccb(xor(level, encbstate)) encastate=level end) --level doesnt seem to work
+    gpio.mode(encqb, gpio.INT, gpio.PULLUP)
+    gpio.trig(encqb, "both", function(level) level=gpio.read(encqb)==0 enccb(not xor(encastate, level)) encbstate=level end) --level doesnt seem to work
+    encastate = gpio.read(encqa)==0
+    encbstate = gpio.read(encqb)==0
+end
     i2c.setup(0, sdapin, sclpin, i2c.SLOW)
     disp = u8g.ssd1306_128x64_i2c(dispaddr)
 
@@ -160,7 +188,7 @@ end
 setup()
 doScreen()
 tmr.alarm(0, 400, 1, doWdt) 
-tmr.alarm(1, 333, 1, doTemp) 
+tmr.alarm(1, 1000, 1, doTemp) 
 tmr.alarm(2, 100, 1, function() if dispwstat ~= wifi.sta.status() or dispwstat == wifi.STA_CONNECTING then doScreen() end end ) 
 
 --=adc.readvdd33()

aca4d074e6bd49e13fbb06d8664575440eb76bdc
Mark Finn <mfinn@fedex.com>
2016-01-09 11:23:37 -0600

    working telnet server for esplorer. added echo. link to fake pty serial port with sudo socat pty,link=/dev/ttyUSB5,raw,user=root,group=dialout,mode=0660 tcp:192.168.13.120:2323

--- init.lua
+++ init.lua
@@ -1,7 +1,16 @@
-function startup()
-    print('startup.')
-    dofile("telnet.lua")
+function startup2()
+    print('main startup.')
     require('script2')
     end
-print('starting in 5 seconds. send tmr.stop(0) to abort')
-tmr.alarm(0,5000,0,startup)
+
+function startup1()
+    print('telnet startup.')
+    require("telnet")
+    telnetserver(function() tmr.stop(0) end)
+    telnetserver = nil
+    print('starting main in 3 seconds. send tmr.stop(0) to abort')
+    tmr.alarm(0,3000,0,startup2)
+    end
+print('starting telnet server in 2 seconds. send tmr.stop(0) to abort')
+tmr.alarm(0,2000,0,startup1)
+

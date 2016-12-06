981708929e23a0eef899fc1573b02bab9ad3990e
Mark Finn <mark@mfinn.net>
2016-12-06 08:50:58 -0600

    turing on telnet server

--- init.lua
+++ init.lua
@@ -5,12 +5,12 @@ function startup2()
     end
 
 function startup1()
---    print('telnet startup.')
---    require("telnet")
---    telnetserver(function() tmr.stop(0) end)
+    print('telnet startup.')
+    require("telnet")
+    telnetserver(function() tmr.stop(0) end)
     telnetserver = nil
-    print('starting main in 3 seconds. send tmr.stop(0) to abort')
-    tmr.alarm(0,3000,0,startup2)
+    print('starting main in 10 seconds. send tmr.stop(0) to abort')
+    tmr.alarm(0,10000,0,startup2)
     end
 print(node.bootreason())
     
